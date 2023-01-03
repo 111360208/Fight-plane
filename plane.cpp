9eefe6e18609576216f8958eab0de54a1d97060f
123 <t111360203@ntut.org.tw>
2023-01-04 00:42:22 +0800

    library

--- plane.cpp
+++ plane.cpp
@@ -1,3 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>    
+#include <stdlib.h>
+#include <conio.h> 
+#include <windows.h>
+#include <time.h>
 
+#define high 25
+#define width 30
+#define border 0 
+#define blank 1
+#define plane 2
+#define bullet 3
+#define enemy 4
+#define destroy 5
+
+int canvas[high+2][width+2];
+int pos_h,pos_w;
+int enemynum;
+int interval;
+int itv_move;
+int itv_new;
+int score; 
+int IsOver;
